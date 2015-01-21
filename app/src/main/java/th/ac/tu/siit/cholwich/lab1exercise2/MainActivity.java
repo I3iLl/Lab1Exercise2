8501e15485bad2fb713aa3a1c0b0816b5b29f803
Charlie Deesarapad <charlie2544@hotmail.com>
2015-01-21 11:20:19 +0700

    5522792274 Charlie Deesarapad

--- app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -20,6 +24,60 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void buttonClick(View v){
+        EditText etInput = (EditText)findViewById(R.id.etInput);
+        String s = etInput.getText().toString();
+        Double tem = Double.parseDouble(s);
+
+        RadioGroup rgFrom = (RadioGroup)findViewById(R.id.rgFrom);
+        int selFrom = rgFrom.getCheckedRadioButtonId();
+
+        RadioGroup rgTo = (RadioGroup)findViewById(R.id.rgTo);
+        int selTo = rgTo.getCheckedRadioButtonId();
+
+        if(selFrom == R.id.rbFrmC){
+
+            if(selTo == R.id.rbToC){
+                //tem = tem;
+            }
+            else if(selTo == R.id.rbToF){
+                tem = (tem * 9/5) + 32;
+            }
+            else if(selTo == R.id.rbToK){
+                tem = tem + 273.15;
+            }
+        }
+
+        else if(selFrom == R.id.rbFrmF){
+
+            if(selTo == R.id.rbToC){
+                tem = (tem - 32) * 5/9;
+            }
+            else if(selTo == R.id.rbToF){
+                //tem = tem;
+            }
+            else if(selTo == R.id.rbToK){
+                tem = ((tem - 32) * 5/9) + 273.15;
+            }
+        }
+
+        else if(selFrom == R.id.rbFrmK){
+
+            if(selTo == R.id.rbToC){
+                tem = tem - 273.15;
+            }
+            else if(selTo == R.id.rbToF){
+                tem = (((tem - 273.15) * 9)/5)+ 32;
+            }
+            else if(selTo == R.id.rbToK){
+                //tem = tem;
+            }
+        }
+
+        TextView tv = (TextView)findViewById(R.id.tvOutput);
+        tv.setText(String.format("%.2f",tem));
     }
 
     @Override
